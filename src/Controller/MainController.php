c7a2a9b9253dba543c55bb66f6388ae1ac99d913
a.bovsunivskiy <bovsunivskiy@gmail.com>
2020-09-11 15:55:30 +0300

    add base html

---
+++ src/Controller/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class MainController extends AbstractController
+{
+    /**
+     * @Route ("/", methods={"GET"}, name="app_main")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig');
+
+    }
+
+}
